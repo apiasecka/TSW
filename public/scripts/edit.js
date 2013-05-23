bbfc9dc86eaa9a447fe070c1e020e0515068670f
apiasecka <alexa1401@gmail.com>
2013-05-23 23:41:02 +0200

    widocznosc naniesionych zmian/tekstu

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -5,3 +5,11 @@ var Editor = function(socket) {
 Editor.prototype.UserNick = function(nick){
 	this.socket.emit('nick', nick);
 };
+
+Editor.prototype.ChangeDoc = function(key, start, startNode){
+	this.socket.emit('key', {
+		'key': key, 
+		'start': start, 
+		'startNode': startNode
+	});
+};
